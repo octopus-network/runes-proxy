38f9e3f885eaa0c9428ac17e28a3052cb809917f
Davirain <davirain.yin@gmail.com>
2024-01-15 18:18:21 +0800

    add some api

--- src/routers/mod.rs
+++ src/routers/mod.rs
@@ -21,8 +21,20 @@ pub fn router() -> Router {
         //.hoop(_cors_handler)
         .hoop(Logger::new())
         .hoop(CatchPanic::new())
-        .push(Router::with_path("runes/blocktime").post(runes::blocktime))
-        .push(Router::with_path("runes/tx").post(runes::tx))
+        .push(Router::with_path("r/block").post(runes::block))
+        .push(Router::with_path("r/blockcount").post(runes::blockcount))
+        .push(Router::with_path("r/blockhash").post(runes::blockhash))
+        .push(Router::with_path("r/blockhash_by_height").post(runes::blockhash_by_height))
+        .push(Router::with_path("r/blockheight").post(runes::blockheight))
+        .push(Router::with_path("r/blocks").post(runes::blocks))
+        .push(Router::with_path("r/blocktime").post(runes::blocktime))
+        .push(Router::with_path("r/clock").post(runes::clock))
+        .push(Router::with_path("r/collections").post(runes::collections))
+        .push(Router::with_path("r/inscriptions").post(runes::inscriptions))
+        .push(Router::with_path("r/tx").post(runes::tx))
+        .push(Router::with_path("r/runes").post(runes::runes))
+        .push(Router::with_path("r/rune").post(runes::rune))
+        .push(Router::with_path("r/status").post(runes::status))
         .append(&mut static_routers);
     let doc = OpenApi::new("salvo web api", "0.0.1").merge_router(&router);
     router
--- src/routers/runes.rs
+++ src/routers/runes.rs
@@ -4,6 +4,111 @@ use crate::constant::TEST_NET_RUNES_RPC;
 use salvo::oapi::endpoint;
 use salvo::oapi::extract::*;
 
+///
+#[endpoint]
+pub async fn block(query: JsonBody<RuneRequest<String>>) -> AppResult<String> {
+    let client = reqwest::Client::new();
+    let response = client
+        .get(format!("{}/block/{}", TEST_NET_RUNES_RPC, query.0.request))
+        .header(reqwest::header::ACCEPT, "application/json")
+        .send()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    // Check if the response is successful
+    if response.status().is_success() {
+        let body = response.text().await.map_err(|e| anyhow::anyhow!(e))?;
+
+        // Parse the response text as JSON
+        Ok(serde_json::from_str::<serde_json::Value>(&body)
+            .map_err(|e| anyhow::anyhow!(e))
+            .map(|v| v.to_string())?)
+    } else {
+        // Handle non-successful response
+        Err(anyhow::anyhow!(response.status()).into())
+    }
+}
+
+/// Get the current block count
+#[endpoint]
+pub async fn blockcount() -> AppResult<String> {
+    let body = reqwest::get(format!("{}/blockcount", TEST_NET_RUNES_RPC))
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?
+        .text()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    Ok(body)
+}
+
+/// Get the current block hash
+#[endpoint]
+pub async fn blockhash() -> AppResult<String> {
+    let body = reqwest::get(format!("{}/r/blockhash", TEST_NET_RUNES_RPC))
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?
+        .text()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    Ok(body)
+}
+
+/// Get the current block hash
+#[endpoint]
+pub async fn blockhash_by_height(height: JsonBody<RuneRequest<String>>) -> AppResult<String> {
+    let body = reqwest::get(format!(
+        "{}/blockhash/{}",
+        TEST_NET_RUNES_RPC, height.0.request
+    ))
+    .await
+    .map_err(|e| anyhow::anyhow!(e))?
+    .text()
+    .await
+    .map_err(|e| anyhow::anyhow!(e))?;
+
+    Ok(body)
+}
+
+/// Get the current block hash
+#[endpoint]
+pub async fn blockheight() -> AppResult<String> {
+    let body = reqwest::get(format!("{}/r/blockheight", TEST_NET_RUNES_RPC))
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?
+        .text()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    Ok(body)
+}
+
+/// Get the current block hash
+#[endpoint]
+pub async fn blocks() -> AppResult<String> {
+    let client = reqwest::Client::new();
+    let response = client
+        .get(format!("{}/blocks", TEST_NET_RUNES_RPC))
+        .header(reqwest::header::ACCEPT, "application/json")
+        .send()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    // Check if the response is successful
+    if response.status().is_success() {
+        let body = response.text().await.map_err(|e| anyhow::anyhow!(e))?;
+
+        // Parse the response text as JSON
+        Ok(serde_json::from_str::<serde_json::Value>(&body)
+            .map_err(|e| anyhow::anyhow!(e))
+            .map(|v| v.to_string())?)
+    } else {
+        // Handle non-successful response
+        Err(anyhow::anyhow!(response.status()).into())
+    }
+}
+
 /// Get the current block height
 #[endpoint]
 pub async fn blocktime() -> AppResult<String> {
@@ -15,6 +120,69 @@ pub async fn blocktime() -> AppResult<String> {
         .map_err(|e| anyhow::anyhow!(e))?;
 
     Ok(body)
+}
+
+/// Get the current block height
+#[endpoint]
+pub async fn clock() -> AppResult<String> {
+    let body = reqwest::get(format!("{}/clock", TEST_NET_RUNES_RPC))
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?
+        .text()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    Ok(body)
+}
+
+/// Get the current block hash
+#[endpoint]
+pub async fn collections() -> AppResult<String> {
+    let client = reqwest::Client::new();
+    let response = client
+        .get(format!("{}/collections", TEST_NET_RUNES_RPC))
+        .header(reqwest::header::ACCEPT, "application/json")
+        .send()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    // Check if the response is successful
+    if response.status().is_success() {
+        let body = response.text().await.map_err(|e| anyhow::anyhow!(e))?;
+
+        // Parse the response text as JSON
+        Ok(serde_json::from_str::<serde_json::Value>(&body)
+            .map_err(|e| anyhow::anyhow!(e))
+            .map(|v| v.to_string())?)
+    } else {
+        // Handle non-successful response
+        Err(anyhow::anyhow!(response.status()).into())
+    }
+}
+
+/// Get the current block hash
+#[endpoint]
+pub async fn inscriptions() -> AppResult<String> {
+    let client = reqwest::Client::new();
+    let response = client
+        .get(format!("{}/inscriptions", TEST_NET_RUNES_RPC))
+        .header(reqwest::header::ACCEPT, "application/json")
+        .send()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    // Check if the response is successful
+    if response.status().is_success() {
+        let body = response.text().await.map_err(|e| anyhow::anyhow!(e))?;
+
+        // Parse the response text as JSON
+        Ok(serde_json::from_str::<serde_json::Value>(&body)
+            .map_err(|e| anyhow::anyhow!(e))
+            .map(|v| v.to_string())?)
+    } else {
+        // Handle non-successful response
+        Err(anyhow::anyhow!(response.status()).into())
+    }
 }
 
 #[endpoint]
@@ -39,4 +207,68 @@ pub async fn tx(txid: JsonBody<RuneRequest<String>>) -> AppResult<String> {
         // Handle non-successful response
         Err(anyhow::anyhow!(response.status()).into())
     }
+}
+
+#[endpoint]
+pub async fn rune(rune_id: JsonBody<RuneRequest<String>>) -> AppResult<String> {
+    let client = reqwest::Client::new();
+    let response = client
+        .get(format!(
+            "{}/runes/{}",
+            TEST_NET_RUNES_RPC, rune_id.0.request
+        ))
+        .header(reqwest::header::ACCEPT, "application/json")
+        .send()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    // Check if the response is successful
+    if response.status().is_success() {
+        let body = response.text().await.map_err(|e| anyhow::anyhow!(e))?;
+
+        // Parse the response text as JSON
+        Ok(serde_json::from_str::<serde_json::Value>(&body)
+            .map_err(|e| anyhow::anyhow!(e))
+            .map(|v| v.to_string())?)
+    } else {
+        // Handle non-successful response
+        Err(anyhow::anyhow!(response.status()).into())
+    }
+}
+
+#[endpoint]
+pub async fn runes() -> AppResult<String> {
+    let client = reqwest::Client::new();
+    let response = client
+        .get(format!("{}/runes", TEST_NET_RUNES_RPC))
+        .header(reqwest::header::ACCEPT, "application/json")
+        .send()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    // Check if the response is successful
+    if response.status().is_success() {
+        let body = response.text().await.map_err(|e| anyhow::anyhow!(e))?;
+
+        // Parse the response text as JSON
+        Ok(serde_json::from_str::<serde_json::Value>(&body)
+            .map_err(|e| anyhow::anyhow!(e))
+            .map(|v| v.to_string())?)
+    } else {
+        // Handle non-successful response
+        Err(anyhow::anyhow!(response.status()).into())
+    }
+}
+
+/// Get the current block height
+#[endpoint]
+pub async fn status() -> AppResult<String> {
+    let body = reqwest::get(format!("{}/status", TEST_NET_RUNES_RPC))
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?
+        .text()
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    Ok(body)
 }
